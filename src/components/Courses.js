03ef1f5ad38499456427bf78e132e22fc32652b7
Liz Trejo <liz@liztrejo.dev>
2023-09-28 23:02:22 -0700

    adds error handling to courses and hides New Course card for unauthorized users

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,24 +1,32 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { api } from '../utils/apiHelper'
+import UserContext from '../context/UserContext'
 import CourseCard from './CourseCard'
 import CourseAddCard from './CourseAddCard'
 import Loading from './Loading'
 
 const Courses = () => {
-
   const [courses, setCourses] = useState([])
   const [loading, setLoading] = useState(true)
-
-  const fetchCourses = async () => {
-    const res = await api('/courses')
-    const data = await res.json()
-    setCourses(data)
-    setLoading(false)
-  }
+  const navigate = useNavigate()
+  const { authUser } = useContext(UserContext);
 
   useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const res = await api('/courses')
+        const data = await res.json()
+        setCourses(data)
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+        navigate('/error')
+      }
+    }
+    
     fetchCourses()
-  }, [])
+  }, [navigate])
 
   const CourseListing = 
     courses.map(course => <CourseCard course={course} key={course.id} />)
@@ -26,8 +34,8 @@ const Courses = () => {
   return (
     <main>
       <div className="wrap main--grid">
-        { loading ? <Loading /> : 
-          <> {CourseListing}<CourseAddCard /> </> }
+        { loading ? <Loading /> : <>{CourseListing}</> }
+        { (!loading && authUser) ? <CourseAddCard /> : null }
       </div>
     </main>
   )
