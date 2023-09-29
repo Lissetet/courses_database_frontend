a143111537937dfd67555efc3a0aa3939c9a8d78
Liz Trejo <liz@liztrejo.dev>
2023-09-28 22:59:35 -0700

    corrects class to classname

--- src/components/ActionsBar.js
+++ src/components/ActionsBar.js
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 const ActionsBar = ({id, handleDelete}) => {
   return (
-    <div class="actions--bar">
-      <div class="wrap">
-          <Link class="button" to={`/courses/${id}/update`}>Update Course</Link>
-          <button class="button" onClick={handleDelete}>Delete Course</button>
-          <Link class="button button-secondary" to="/">Return to List</Link>
+    <div className="actions--bar">
+      <div className="wrap">
+          <Link className="button" to={`/courses/${id}/update`}>Update Course</Link>
+          <button className="button" onClick={handleDelete}>Delete Course</button>
+          <Link className="button button-secondary" to="/">Return to List</Link>
       </div>
     </div>
   )
